8c70e854eab5cced4c6195ea7e3494f45c603bc1
Christian Daher <christian@tomorrow.services>
2023-12-26 11:46:12 +0200

    more dummy data

--- src/main/resources/db/migration/V4__Seed_collections_table.sql
+++ src/main/resources/db/migration/V4__Seed_collections_table.sql
@@ -1,3 +1,5 @@
 INSERT INTO collections (user_id, title, category) VALUES 
-(1, 'JSP', 'Programming 3'),
-(1, 'Servlets', 'Programming 3');
+(1, 'Programming', 'major'),
+(1, 'Machine Learning', 'major'),
+(1, 'Numerical Methods', 'minor'),
+(1, 'French', 'elective');
--- src/main/resources/db/migration/V6__Seed_flashcards_table.sql
+++ src/main/resources/db/migration/V6__Seed_flashcards_table.sql
@@ -1,7 +1,145 @@
-INSERT INTO flashcards (collection_id, question, answer) VALUES 
-(1, 'What does JSP stand for?', 'Java Server Page'),
-(1, 'What does API stand for?', 'Application Programming Interface'),
-(1, 'What does JPA stand for?', 'Java Persistence API'),
-(1, 'What does HTTP stand for?', 'HyperText Transfer Protocol'),
-(1, 'What does HTML stand for?', 'HyperText Markup Language'),
-(1, 'What does CSS stand for?', 'Cascading Style Sheets');
+INSERT INTO
+    flashcards (collection_id, question, answer)
+VALUES
+    (
+        1,
+        'What does JSP stand for?',
+        'Java Server Page'
+    ),
+    (
+        1,
+        'What does API stand for?',
+        'Application Programming Interface'
+    ),
+    (
+        1,
+        'What does JPA stand for?',
+        'Java Persistence API'
+    ),
+    (
+        1,
+        'What does HTTP stand for?',
+        'HyperText Transfer Protocol'
+    ),
+    (
+        1,
+        'What does HTML stand for?',
+        'HyperText Markup Language'
+    ),
+    (
+        1,
+        'What does CSS stand for?',
+        'Cascading Style Sheets'
+    ),
+    (2, 'What does ML stand for?', 'Machine Learning'),
+    (2, 'What does DM stand for?', 'Data Mining'),
+    (
+        2,
+        'What does LLM stand for?',
+        'Large Language Model'
+    ),
+    (
+        2,
+        'What is supervised learning about?',
+        'In supervised learning, the algorithm is trained on a labeled dataset, where the input data is paired with the corresponding correct output. The model learns to map inputs to outputs, making predictions on new, unseen data.'
+    ),
+    (
+        2,
+        'What is unsupervised learning about?',
+        'Unsupervised learning involves training a model on an unlabeled dataset. The algorithm must find patterns and relationships in the data without explicit guidance on the correct output.'
+    ),
+    (
+        2,
+        'What is a Neural Network?',
+        'A neural network is a computational model inspired by the structure and functioning of the human brain. It consists of interconnected nodes (neurons) organized in layers, including an input layer, hidden layers, and an output layer.'
+    ),
+    (
+        2,
+        'What is deep learning?',
+        'Deep Learning is a subset of machine learning that involves neural networks with multiple layers (deep neural networks). It is particularly effective in handling large amounts of data and is used in tasks such as image and speech recognition.'
+    ),
+    (
+        2,
+        'When does overfitting occur?',
+        'Overfitting occurs when a model learns the training data too well, capturing noise and irrelevant details. This can result in poor performance on new, unseen data.'
+    ),
+    (
+        2,
+        'When does underfitting happen?',
+        'Underfitting happens when a model is too simple to capture the underlying patterns in the data, leading to poor performance on both the training and new data.'
+    ),
+    (
+        2,
+        'What is a Decision Tree?',
+        'A decision tree is a tree-like model where each node represents a decision or a test on an attribute, each branch represents an outcome of the test, and each leaf node represents a class label or a decision.'
+    ),
+    (
+        3,
+        'What is the Bisection Method?',
+        'The Bisection Method is a numerical technique for finding the root of a real-valued function. It works by repeatedly narrowing down an interval where the root is located until a sufficiently accurate solution is obtained.'
+    ),
+    (
+        3,
+        'What does the term "Convergence" mean in Numerical Methods?',
+        'Convergence refers to the property of an iterative numerical method where the solution approaches a stable and accurate value as the algorithm progresses through successive iterations.'
+    ),
+    (
+        3,
+        'What is Gaussian Elimination?',
+        'Gaussian Elimination is a method used for solving systems of linear equations. It involves transforming the augmented matrix into row-echelon form and then back-substituting to find the solution.'
+    ),
+    (
+        3,
+        'What is the Newton-Raphson Method?',
+        'The Newton-Raphson Method is an iterative technique for finding the roots of a real-valued function. It uses linear approximation and updates the current guess based on the function and its derivative.'
+    ),
+    (
+        3,
+        'What is the Purpose of Interpolation in Numerical Methods?',
+        'Interpolation is used to estimate values between known data points. In numerical methods, interpolation techniques are employed to construct functions that pass through given sets of data points.'
+    ),
+    (
+        4,
+        'What does "Bonjour" mean?',
+        '"Bonjour" translates to "Hello" or "Good morning" in English. It is a common greeting in French used primarily in the morning and early afternoon.'
+    ),
+    (
+        4,
+        'Translate "Merci" to English.',
+        '"Merci" translates to "Thank you" in English. It is used to express gratitude or appreciation.'
+    ),
+    (
+        4,
+        'What is the meaning of "La Plume de Ma Tante"?',
+        '"La Plume de Ma Tante" is a French phrase that means "The pen of my aunt." It is often used as an example of a nonsensical or humorous phrase for language learners.'
+    ),
+    (
+        4,
+        'Translate "Bonne Nuit" to English.',
+        '"Bonne Nuit" translates to "Good night" in English. It is a common expression used when wishing someone a good night.'
+    ),
+    (
+        4,
+        'What does "Voulez-vous coucher avec moi ce soir?" mean?',
+        '"Voulez-vous coucher avec moi ce soir?" translates to "Do you want to sleep with me tonight?" This phrase is famously known from the song "Lady Marmalade."'
+    ),
+    (
+        4,
+        'What does "Croissant" mean?',
+        '"Croissant" translates to "Crescent" in English. It is a popular French pastry known for its crescent shape.'
+    ),
+    (
+        4,
+        'Translate "Château" to English.',
+        '"Château" translates to "Castle" in English. It typically refers to a large French country house or castle.'
+    ),
+    (
+        4,
+        'What is the French equivalent of "Yes"?',
+        'The French equivalent of "Yes" is "Oui."'
+    ),
+    (
+        4,
+        'Translate "Au Revoir" to English.',
+        '"Au Revoir" translates to "Goodbye" in English. It is a polite way to bid farewell.'
+    )
